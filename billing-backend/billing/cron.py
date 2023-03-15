c7a6882496bb96a350c0274f92692b0361339834
Allen Chen <allen.chen@hiero7.com>
2023-03-15 10:57:27 +0800

    add cron

--- billing-backend/billing/cron.py
+++ billing-backend/billing/cron.py
@@ -1,6 +1,7 @@
 import datetime
 import tools.dbhelper as dbhelper
 import json
+import ast
 
 
 class RunReport:
@@ -59,7 +60,108 @@ class RunReport:
             mysql.execute(sql, params)
 
     def save_aggregates_reports(self):
-        print('save_aggregates_reports')
+        now_datetime, sdate, edate, year, month, monthName = self.get_last_month_start_end()
+
+        mysql = dbhelper.DBHelper()
+        sql = "select * from billing_billingsettingaggregates where permanent != 'manual-one-time'"
+        bw_prefix_set_group = mysql.fetch(sql)
+
+        for group in bw_prefix_set_group:
+            self.save_aggregate_results(now_datetime, year, month, group)
+
+        # send email
+
+    def save_aggregate_results(self, now_datetime, year, month, group_row):
+        mysql = dbhelper.DBHelper()
+        group_name = group_row['group_name']
+        group_ary = group_row['billing_list']
+        group_pir = group_row['pir']
+        permanent = group_row['permanent']
+
+        sql = "select * from billing_billingsummaryaggregates where group_name = %s and year = %s and month = %s order by id desc"
+        params = (group_name, year, month)
+        last_row = mysql.fetchone(sql, params)
+        sequence = 1
+
+        if last_row is not None:
+            sequence = last_row['sequence']
+            sequence = sequence + 1
+
+        billing_info = dict(
+            group_name=group_name,
+            year=year,
+            month=month,
+            billing_list=group_ary,
+            percentile_98_h=0,
+            percentile_98_hm=0,
+            percentile_98=0,
+            percentile_98_m=0,
+            percentile_mbps_98=0,
+            data_progress='1/' + str(len(ast.literal_eval(group_ary))),
+            monthly_report_done=0,
+            sequence=sequence,
+            created_at=now_datetime,
+            updated_at=now_datetime
+
+        )
+
+        sql = """
+                INSERT INTO billing_billingsummaryaggregates(group_name, year, month, billing_list, percentile_98_h, percentile_98_hm, percentile_98, percentile_98_m, percentile_mbps_98, data_progress, monthly_report_done, sequence, created_at, updated_at) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+              """
+        params = (billing_info['group_name'],
+                  billing_info['year'],
+                  billing_info['month'],
+                  billing_info['billing_list'],
+                  billing_info['percentile_98_h'],
+                  billing_info['percentile_98_hm'],
+                  billing_info['percentile_98'],
+                  billing_info['percentile_98_m'],
+                  billing_info['percentile_mbps_98'],
+                  billing_info['data_progress'],
+                  billing_info['monthly_report_done'],
+                  billing_info['sequence'],
+                  billing_info['created_at'],
+                  billing_info['updated_at'],
+                  )
+        mysql.execute(sql, params)
+        curr_id = mysql.cur.lastrowid
+
+        sql = "select a.sensor_id, a.prefix_list, b.billing_id from billing_sensor as a left join billing_billingsetting as b on a.billing_settings_id = b.id where billing_id in " + str(group_ary).replace('[', '(').replace(']', ')')
+        result = mysql.fetch(sql)
+
+        sensor_list = []
+        prefix_list = []
+        for raw in result:
+            sensor_list.append(raw['sensor_id'])
+            prefix_list.append(raw['prefix_list'])
+
+        bandwidth_charging_id = 'Aggregate-' + group_name
+
+        if len(sensor_list) > 0 and len(prefix_list) > 0:
+            percentile_98_h = 0;
+            percentile_98_hm = 0;
+            percentile_98 = 0;
+            percentile_98_m = 0;
+
+            percentile_98, percentile_98_h = self.get_multi_percentile(sensor_list, 98, None, prefix_list,bandwidth_charging_id)
+            percentile_98_m, percentile_98_hm = self.get_multi_percentile(sensor_list, 98, None, prefix_list,bandwidth_charging_id)
+
+    def get_multi_percentile(self, sensor_ids, percentile=98, time_list=None, prefixes_by_in_out=None, bandwidth_charging_id=None):
+        multi_percentile = None
+        incoming_percentile = None
+        outgoing_percentile = None
+        sampling_in_x_minute = 1
+        historicData = None
+
+        historicData = self.get_samples_by_month(sensor_ids, time_list, prefixes_by_in_out, table='historic')
+        return 1, 2
+
+    def get_samples_by_month(self,sensor_ids, time_list, prefixes_by_in_out, table='historic'):
+        mysql = dbhelper.DBHelper()
+
+        sql ='select ANY_VALUE(SUM(incoming)) as incoming, ANY_VALUE(SUM(outgoing)) as outgoing, datetime from ' + table + ' where sensor_id in ' + str(sensor_ids).replace('[', '(').replace(']', ')') + ' and prefix in ' + str(prefixes_by_in_out).replace('["', '').replace('"]', '').replace('[', '(').replace(']', ')') + ' group by datetime'
+        result = mysql.fetch(sql)
+        print(result)
 
     def copy_historic_data(self):
         now_datetime, sdate, edate, year, month, monthName = self.get_last_month_start_end()
